514dd8e67367f75f41949a3ca1db38d8cd773e50
Patrick Roberts <patrickroberts93@gmail.com>
2017-04-17 17:43:26 +1000

    resolves issue #10 - created hamburger menu for small screen size

--- js/main.js
+++ js/main.js
@@ -3,4 +3,11 @@ $( document ).ready(function() {
 
   $('.flexslider').flexslider();
 
+  jQuery('.toggle-nav').click(function(e) {
+        jQuery(this).toggleClass('active');
+        jQuery('.menu ul').toggleClass('active');
+
+        e.preventDefault();
+    });
+
 });
